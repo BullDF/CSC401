81293afb6a302ee9fbee11e1fa87279da79e9f40
Johnny Meng <jmyw139@gmail.com>
2024-04-03 14:04:53 -0400

    Finished 2.1

--- a3/a3_levenshtein.py
+++ a3/a3_levenshtein.py
@@ -6,10 +6,12 @@ import numpy as np
 
 dataDir = '/u/cs401/A3/data/'
 
+
 # Class definition for an individual table cell. DO NOT MODIFY
 class TableCell(object):
     EditTypes = Literal['del', 'ins', 'match', 'sub', 'first']
-    def __init__(self, cost:int = 0, trace:EditTypes = 'first'):
+
+    def __init__(self, cost: int = 0, trace: EditTypes = 'first'):
         """
         initialize: defines an individual table cell
 
@@ -37,6 +39,7 @@ class TableCell(object):
         super().__init__()
         self.cost, self.trace = cost, trace
 
+
 # Preprocess text for best result. DO NOT MODIFY!
 def preprocess(raw_line):
     # remove label
@@ -53,8 +56,9 @@ def preprocess(raw_line):
 
     return line
 
+
 # Complete the following component for Levenshtein
-def initialize(U : int, T : int) -> List[List[TableCell]]:
+def initialize(U: int, T: int) -> List[List[TableCell]]:
     """
     initialize: allocate the table and initialize the first row and column
 
@@ -71,15 +75,27 @@ def initialize(U : int, T : int) -> List[List[TableCell]]:
     """
 
     ############################################
-    table = None
-    print('TODO')
+    table = []
+    for u in range(U + 1):
+        row = []
+        for t in range(T + 1):
+            if u == 0:
+                cell = TableCell(t)
+            elif t == 0:
+                cell = TableCell(u)
+            else:
+                cell = TableCell()
+            row.append(cell)
+        table.append(row)
     ############################################
 
     # Make sure the type of values are correct
-    assert isinstance(table, list) and all(isinstance(row, list) and all(isinstance(cell, TableCell) for cell in row) for row in table)
+    assert isinstance(table, list) and all(
+        isinstance(row, list) and all(isinstance(cell, TableCell) for cell in row) for row in table)
     return table
 
-def step(u : int, t : int, table : List[List[TableCell]], r : List[str], h : List[str]) -> None:
+
+def step(u: int, t: int, table: List[List[TableCell]], r: List[str], h: List[str]) -> None:
     """
     step: computes the value of the current cell
 
@@ -98,9 +114,27 @@ def step(u : int, t : int, table : List[List[TableCell]], r : List[str], h : Lis
     N/A
     """
     ############################################
-    print('TODO')
+    match = table[u - 1][t - 1].cost
+    substitution = table[u - 1][t - 1].cost + 1
+    insertion = table[u][t - 1].cost + 1
+    deletion = table[u - 1][t].cost + 1
+
+    if r[u - 1] == h[t - 1]:
+        table[u][t].cost = min(match, insertion, deletion)
+    else:
+        table[u][t].cost = min(substitution, insertion, deletion)
+
+    if table[u][t].cost == match:
+        table[u][t].trace = 'match'
+    elif table[u][t].cost == substitution:
+        table[u][t].trace = 'sub'
+    elif table[u][t].cost == insertion:
+        table[u][t].trace = 'ins'
+    else:
+        table[u][t].trace = 'del'
     ############################################
     return
+
 
 def finalize(table):
     """
@@ -117,16 +151,31 @@ def finalize(table):
     (WER, nI, nD, nS): (float, int, int, int) WER, number of insertions, deletions, and substitutions respectively
     """
     # Define results to be returned:
-    wer = 0.0
-    insertions = 0
-    deletions = 0
-    substitutions = 0
 
     ############################################
-    print('TODO')
+    U, T = len(table) - 1, len(table[0]) - 1
+    wer = float('inf') if U == 0 else table[U][T].cost / U
+
+    backward = {'match': [0, (1, 1)], 'sub': [0, (1, 1)], 'ins': [0, (0, 1)], 'del': [0, (1, 0)]}
+
+    def trace(u, t):
+        if table[u][t].trace == 'first':
+            backward['ins'][0] += t
+            backward['del'][0] += u
+            return
+
+        backward[table[u][t].trace][0] += 1
+        du, dt = backward[table[u][t].trace][1]
+        return trace(u - du, t - dt)
+
+    trace(U, T)
+    insertions = backward['ins'][0]
+    deletions = backward['del'][0]
+    substitutions = backward['sub'][0]
     ############################################
 
     return (wer, insertions, deletions, substitutions)
+
 
 def Levenshtein(r, h):
     """
@@ -146,12 +195,12 @@ def Levenshtein(r, h):
 
     Examples
     --------
-    >>> wer("who is there".split(), "is there".split())
-    0.333 0 1 0
-    >>> wer("who is there".split(), "".split())
-    1.0 0 3 0
-    >>> wer("".split(), "who is there".split())
-    Inf 3 0 0
+    >>> Levenshtein("who is there".split(), "is there".split())
+    (0.3333333333333333, 0, 1, 0)
+    >>> Levenshtein("who is there".split(), "".split())
+    (1.0, 0, 3, 0)
+    >>> Levenshtein("".split(), "who is there".split())
+    (inf, 3, 0, 0)
     """
 
     # U: length of reference;
@@ -175,6 +224,7 @@ def Levenshtein(r, h):
     # A.K.A WER, insertions, deletions and substitutions
     return finalize(table)
 
+
 if __name__ == "__main__":
     """
     Main Function: Generates a file that has all the result. DO NOT MODIFY! 
@@ -194,6 +244,7 @@ if __name__ == "__main__":
     GOOGLE = "transcripts.Google.txt"
     KALDI = "transcripts.Kaldi.txt"
 
+
     # Make sure the data is clean and usable
     def check_valid(sdir):
         def exists(name):
@@ -201,6 +252,7 @@ if __name__ == "__main__":
             return os.path.exists(path)
 
         return exists(GTF) or exists(GOOGLE) or exists(KALDI)
+
 
     # Load speakers names
     speakers = os.listdir(dataDir)
@@ -238,7 +290,8 @@ if __name__ == "__main__":
                 wer_google.append(google_lev[0])
                 wer_kaldi.append(kaldi_lev[0])
 
-                print(f'{s} Google {i} {google_lev[0]:.6f} I:{google_lev[1]} D:{google_lev[2]} S:{google_lev[3]}', file=file)
+                print(f'{s} Google {i} {google_lev[0]:.6f} I:{google_lev[1]} D:{google_lev[2]} S:{google_lev[3]}',
+                      file=file)
                 print(f'{s} Kaldi {i} {kaldi_lev[0]:.6f} I:{kaldi_lev[1]} D:{kaldi_lev[2]} S:{kaldi_lev[3]}', file=file)
 
         wer_google = np.array(wer_google)
@@ -248,5 +301,3 @@ if __name__ == "__main__":
         print(f"FINAL KALDI AVG = {np.mean(wer_kaldi) :.4f} +- {np.std(wer_kaldi):.4f}", file=file)
         print(f"FINAL GOOGLE WER = {np.mean(wer_google):.4f} +- {np.std(wer_google):.4f}", file=file)
     print("a3_levenshtein process has completed!")
-
-
